4f0d0037e211e4ab49d74c45b0bb9df999303906
aliciaguiradelo <alicia.guiradelo@outlook.com>
2024-04-05 00:50:15 -0300

    thymeleaf

--- src/main/java/br/com/fiap/docschedule/consulta/Consulta.java
+++ src/main/java/br/com/fiap/docschedule/consulta/Consulta.java
@@ -6,6 +6,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,7 +19,10 @@ import lombok.NoArgsConstructor;
 public class Consulta {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotBlank(message = "{task.especialidade.notblank}")
     private String especialidade;
+    @Size(min=10, max=255, message = "{task.motivo.size}")
     private String motivo;
+    @NotBlank(message = "{task.dataConsulta.notblank}")
     private LocalDate dataConsulta;
 }
