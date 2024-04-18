69a7fb45bd52fb54ae777251e7e79ccf515634d2
aliciaguiradelo <alicia.guiradelo@outlook.com>
2024-04-17 21:03:24 -0300

    add import SecurityConfig

--- src/main/java/br/com/fiap/docschedule/config/SecurityConfig.java
+++ src/main/java/br/com/fiap/docschedule/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package br.com.fiap.docschedule.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
